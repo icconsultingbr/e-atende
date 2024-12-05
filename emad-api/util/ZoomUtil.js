74c45b4fb4d24a595dc4847290ef6a9d2aa049d2
eduardo <eduardo@threeo.com.br>
2024-12-05 11:14:28 -0300

    Modificações nas rotas do Zoom

--- emad-api/util/ZoomUtil.js
+++ emad-api/util/ZoomUtil.js
@@ -1,18 +1,42 @@
-const isDefined = (str) => !!str
+// Check if a string is defined (not null or undefined)
+function isDefined(str) {
+  return !!str;
+}
 
-const isStringArray = (value) => Array.isArray(value) && value.every((x) => typeof x === 'string')
+// Check if a value is an array of strings
+function isStringArray(value) {
+  return Array.isArray(value) && value.every(function (x) {
+      return typeof x === 'string';
+  });
+}
 
-const isValidationError = (value) =>
-  value != null && typeof value.property !== 'undefined' && typeof value.reason !== 'undefined'
+// Check if a value is a validation error
+function isValidationError(value) {
+  return value && typeof value.property !== 'undefined' && typeof value.reason !== 'undefined';
+}
 
-const replaceWhitespace = (str) => str.replace(/\s+/g, '')
+// Replace all whitespace in a string
+function replaceWhitespace(str) {
+  return str.replace(/\s+/g, '');
+}
 
-const toStringArray = (value) =>
-  Array.isArray(value)
-    ? value.flatMap(replaceWhitespace).filter(isDefined)
-    : (value != null ? value.split(',').map(replaceWhitespace).filter(isDefined) : [])
+// Convert a value to a string array
+function toStringArray(value) {
+  if (Array.isArray(value)) {
+      return value.reduce(function (acc, item) {
+          return acc.concat(replaceWhitespace(item));
+      }, []).filter(isDefined);
+  } else if (value && typeof value === 'string') {
+      return value.split(',').map(replaceWhitespace).filter(isDefined);
+  }
+  return [];
+}
 
 // Export all functions
 module.exports = {
-     isDefined, isStringArray, isValidationError, replaceWhitespace, toStringArray
+  isDefined: isDefined,
+  isStringArray: isStringArray,
+  isValidationError: isValidationError,
+  replaceWhitespace: replaceWhitespace,
+  toStringArray: toStringArray
 };
--- emad-api/util/ZoomValidations.js
+++ emad-api/util/ZoomValidations.js
@@ -1,113 +1,131 @@
-const { isStringArray, isValidationError, toStringArray } = require('./ZoomUtil');
+const ZoomUtil = require('./ZoomUtil');
+const isStringArray = ZoomUtil.isStringArray;
+const isValidationError = ZoomUtil.isValidationError;
+const toStringArray = ZoomUtil.toStringArray;
 
 // CURRIED VALIDATORS
-const inNumberArray = (allowedNumbers) => (property, value) => {
-  if (typeof value === 'undefined') return;
+function inNumberArray(allowedNumbers) {
+    return function (property, value) {
+        if (typeof value === 'undefined') return;
 
-  if (typeof value !== 'number' || isNaN(value)) {
-    return {
-      property,
-      reason: `Value ${value} not allowed, must be of type number`,
+        if (typeof value !== 'number' || isNaN(value)) {
+            return {
+                property: property,
+                reason: 'Value ' + value + ' not allowed, must be of type number'
+            };
+        }
+
+        if (allowedNumbers.indexOf(value) === -1) {
+            return {
+                property: property,
+                reason: 'Value is not valid. Got ' + value + ', expected ' + allowedNumbers
+            };
+        }
     };
-  }
+}
 
-  if (!allowedNumbers.includes(value)) {
-    return {
-      property,
-      reason: `Value is not valid. Got ${value}, expected ${allowedNumbers}`,
+function isBetween(min, max) {
+    return function (property, value) {
+        if (typeof value === 'undefined') return;
+
+        if (typeof value !== 'number' || isNaN(value)) {
+            return {
+                property: property,
+                reason: 'Value ' + value + ' not allowed, must be of type number'
+            };
+        }
+
+        if (value < min || value > max) {
+            return {
+                property: property,
+                reason: 'Value must be between ' + min + ' and ' + max
+            };
+        }
     };
-  }
-};
+}
 
-const isBetween = (min, max) => (property, value) => {
-  if (typeof value === 'undefined') return;
+function isLengthLessThan(maxLength) {
+    return function (property, value) {
+        if (typeof value === 'undefined') return;
 
-  if (typeof value !== 'number' || isNaN(value)) {
-    return {
-      property,
-      reason: `Value ${value} not allowed, must be of type number`,
+        if (typeof value !== 'string') {
+            return {
+                property: property,
+                reason: 'Value ' + value + ' not allowed, must be of type string'
+            };
+        }
+
+        if (value.length > maxLength) {
+            return {
+                property: property,
+                reason: 'Value exceeds max length. Got ' + value.length + ', expected less than or equal to ' + maxLength
+            };
+        }
     };
-  }
+}
 
-  if (value < min || value > max) {
-    return {
-      property,
-      reason: `Value must be between ${min} and ${max}`,
+function isRequired(property, value) {
+    if (typeof value === 'undefined') {
+        return {
+            property: property,
+            reason: 'Property required, but not present in request body'
+        };
+    }
+}
+
+function matchesStringArray(allowedStrings) {
+    return function (property, value) {
+        if (typeof value === 'undefined') return;
+
+        if (typeof value !== 'string' && !isStringArray(value)) {
+            return {
+                property: property,
+                reason: 'Value ' + value + ' not allowed, must be of type string or string array'
+            };
+        }
+
+        const arr = toStringArray(value);
+
+        if (arr.length === 0) {
+            return {
+                property: property,
+                reason: 'Property defined, but no values were present'
+            };
+        }
+
+        if (!arr.every(function (x) { return allowedStrings.indexOf(x) !== -1; })) {
+            return {
+                property: property,
+                reason: 'One or more values not allowed. Got (' + arr + '), expected (' + allowedStrings + ')'
+            };
+        }
     };
-  }
-};
-
-const isLengthLessThan = (maxLength) => (property, value) => {
-  if (typeof value === 'undefined') return;
-
-  if (typeof value !== 'string') {
-    return {
-      property,
-      reason: `Value ${value} not allowed, must be of type string`,
-    };
-  }
-
-  if (value.length > maxLength) {
-    return {
-      property,
-      reason: `Value exceeds max length. Got ${value.length}, expected less than or equal to ${maxLength}`,
-    };
-  }
-};
-
-const isRequired = (property, value) => {
-  if (typeof value === 'undefined') {
-    return {
-      property,
-      reason: `Property required, but not present in request body`,
-    };
-  }
-};
-
-const matchesStringArray = (allowedStrings) => (property, value) => {
-  if (typeof value === 'undefined') return;
-
-  if (typeof value !== 'string' && !isStringArray(value)) {
-    return {
-      property,
-      reason: `Value ${value} not allowed, must be of type string or string array`,
-    };
-  }
-
-  const arr = toStringArray(value);
-
-  if (arr.length === 0) {
-    return {
-      property,
-      reason: `Property defined, but no values were present`,
-    };
-  }
-
-  if (!arr.every((x) => allowedStrings.includes(x))) {
-    return {
-      property,
-      reason: `One or more values not allowed. Got (${arr}), expected (${allowedStrings})`,
-    };
-  }
-};
+}
 
 // VALIDATION RUNNER
-const validateRequest = (body, validator) =>
-  Object.keys(validator)
-    .flatMap((property) => {
-      const value = body ? body[property] : '';
-      const func = validator[property];
-      const validations = Array.isArray(func) ? func.map((f) => f(property, value)) : func(property, value);
-      return Array.isArray(validations) ? validations : [validations];
-    })
-    .filter(isValidationError);
+function validateRequest(body, validator) {
+    return Object.keys(validator)
+        .map(function (property) {
+            var value = body && body[property]; // Replace optional chaining
+            var func = validator[property];
+            var validations = Array.isArray(func)
+                ? func.map(function (f) { return f(property, value); })
+                : func ? func(property, value) : undefined;
+
+            return Array.isArray(validations) ? validations : [validations];
+        })
+        .reduce(function (acc, curr) {
+            return acc.concat(curr);
+        }, [])
+        .filter(isValidationError);
+}
 
 // Export all functions
 module.exports = {
-  inNumberArray,
-  isBetween,
-  isLengthLessThan,
-  isRequired,
-  matchesStringArray,
-  validateRequest,
+    inNumberArray: inNumberArray,
+    isBetween: isBetween,
+    isLengthLessThan: isLengthLessThan,
+    isRequired: isRequired,
+    matchesStringArray: matchesStringArray,
+    validateRequest: validateRequest
 };
